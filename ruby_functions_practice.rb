aa0fa3c2ee48e3285946d4566bd7dde99bd04bf4
Russell Williams <russellwilliamscc@gmail.com>
2016-06-07 22:12:45 +0100

    Adds volume of cube test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -62,3 +62,71 @@ def number_to_full_month_name(month)
    
   end
 end
+
+def number_to_full_month_name(month)
+
+  case month
+  when 1
+    return "January"
+  when 2
+    return "February"  
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
+end
+
+def number_to_short_month_name(month)
+
+  case month
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"  
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  end
+end
+
+def volume_of_cube(length)
+  volume = (length * length * length)
+  return volume
+end
+
+
+
--- specs/ruby_functions_specs.rb
+++ specs/ruby_functions_specs.rb
@@ -49,28 +49,29 @@ class Functions_Practice < MiniTest::Test
 
   def test_number_to_full_name()
     first_month_string = number_to_full_month_name( 1 )
-    # third_month_string = number_to_full_month_name( 3 )
-    # ninth_month_string = number_to_full_month_name( 9 )
+    third_month_string = number_to_full_month_name( 3 )
+    ninth_month_string = number_to_full_month_name( 9 )
     assert_equal( "January", first_month_string )
-    # assert_equal( "March", third_month_string ) 
-    # assert_equal( "September", ninth_month_string )
+    assert_equal( "March", third_month_string ) 
+    assert_equal( "September", ninth_month_string )
   end
 
-  # def test_substring()
-  #   first_month_string = number_to_short_month_name( 1 )
-  #   third_month_string = number_to_short_month_name( 3 )
-  #   ninth_month_string = number_to_short_month_name( 9 )
-  #   assert_equal( "Jan", first_month_string )
-  #   assert_equal( "Mar", third_month_string )
-  #   assert_equal( "Sep", ninth_month_string )
-  # end
+  def test_substring()
+    first_month_string = number_to_short_month_name( 1 )
+    third_month_string = number_to_short_month_name( 3 )
+    ninth_month_string = number_to_short_month_name( 9 )
+    assert_equal( "Jan", first_month_string )
+    assert_equal( "Mar", third_month_string )
+    assert_equal( "Sep", ninth_month_string )
+  end
 
-  # #Further
+  #Further
 
-  # #Given the length of a side of a cube calculate the volume 
-  # def test_volume_of_cube()
-
-  # end
+  #Given the length of a side of a cube calculate the volume 
+  def test_volume_of_cube()
+    result = 3 * 3 * 3 
+    assert_equal(27, result)
+  end
 
   # #Given the radius of a sphere calculate the volume
   # def test_volume_of_sphere()
