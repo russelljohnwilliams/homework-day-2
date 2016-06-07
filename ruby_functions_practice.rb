b552fed93130e5b1b19fabe0ed247244abad6a46
Russell Williams <russellwilliamscc@gmail.com>
2016-06-07 17:18:42 +0100

    testing

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -32,3 +32,32 @@ def add_string_as_number(first_number, second_number)
   return first_number.to_i + second_number.to_i
 end
 
+def number_to_full_name(month)
+
+  case months
+  when 1
+    return "Janurary"
+  when 2
+    return "February"  
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+
+  end
