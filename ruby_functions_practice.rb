e283db4cb4d54b469d2912e880b6c6a3fb1af478
Russell Williams <russellwilliamscc@gmail.com>
2016-06-07 22:54:14 +0100

    adds volume of sphere test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -63,35 +63,6 @@ def number_to_full_month_name(month)
   end
 end
 
-def number_to_full_month_name(month)
-
-  case month
-  when 1
-    return "January"
-  when 2
-    return "February"  
-  when 3
-    return "March"
-  when 4
-    return "April"
-  when 5
-    return "May"
-  when 6
-    return "June"
-  when 7
-    return "July"
-  when 8
-    return "August"
-  when 9
-    return "September"
-  when 10
-    return "October"
-  when 11
-    return "November"
-  when 12
-    return "December"
-  end
-end
 
 def number_to_short_month_name(month)
 
